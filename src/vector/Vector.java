34de5a9b51dcc6a66c7a62333d0bea0b2df69d76
pchmn <pchmn.dev@gmail.com>
2016-11-17 12:13:56 +0100

    classe vector

--- src/vector/Vector.java
+++ src/vector/Vector.java
@@ -1,7 +1,39 @@
 package vector;
 
 
+import java.util.Map;
+
 public class Vector {
 
-    
+    public static double norme(Map<String, Integer> vector) {
+        int result = 0;
+
+        for (Map.Entry<String, Integer> entry : vector.entrySet()) {
+            result += entry.getValue() * entry.getValue();
+        }
+
+        return Math.sqrt(result);
+    }
+
+    public static int produitScalaire(Map<String, Integer> v1, Map<String, Integer> v2) {
+        int result = 0;
+
+        for (Map.Entry<String, Integer> entry : v1.entrySet()) {
+            result += entry.getValue() * v2.get(entry.getKey());
+        }
+
+        return result;
+    }
+
+    public static double cos(Map<String, Integer> v1, Map<String, Integer> v2) {
+        int scalaire = produitScalaire(v1, v2);
+        double normeV1 = norme(v1);
+        double normeV2 = norme(v2);
+
+        return scalaire / (normeV1 * normeV2);
+    }
+
+    public static double angle(Map<String, Integer> v1, Map<String, Integer> v2) {
+        return Math.acos(cos(v1, v2));
+    }
 }
